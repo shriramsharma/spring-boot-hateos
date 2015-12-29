86ad1e3bb3b33434916981af240302d430594f85
Shriram Sharma <ssharma5@walmartlabs.com>
2015-12-28 22:15:43 -0800

    Initial commit

---
+++ src/main/java/com/walmart/platform/spring/boot/hateos/SpringBootHateosApplication.java
@@ -0,0 +1,12 @@
+package com.walmart.platform.spring.boot.hateos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHateosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootHateosApplication.class, args);
+    }
+}
---
+++ src/test/java/com/walmart/platform/spring/boot/hateos/SpringBootHateosApplicationTests.java
@@ -0,0 +1,18 @@
+package com.walmart.platform.spring.boot.hateos;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = SpringBootHateosApplication.class)
+@WebAppConfiguration
+public class SpringBootHateosApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
